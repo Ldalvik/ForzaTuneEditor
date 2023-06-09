4fceff585f3426b6af8d46e5278470ac6bbf472e
Ldalvik <subaruforge@gmail.com>
2023-06-09 17:19:50 -0400

    Support v3 tunes, added tune sort by date created

--- src/TuningAPI/core/TuneFileHandler.js
+++ src/TuningAPI/core/TuneFileHandler.js
@@ -17,32 +17,42 @@ export default class TuneFileHandler {
         return await fs.existsSync(isSteamVersion ? this.STEAM_FORZA_PATH : this.FORZA_PATH)
     }
     async loadTuneFiles(file = null) {
-        const folder = file || this.TUNE_FILES_PATH;
+        const folder = file || this.TUNE_FILES_PATH
         try {
-            const files = await fs.promises.readdir(folder);
+            const files = await fs.promises.readdir(folder)
             for (const fileName of files) {
-                const fileDirectory = `${folder}/${fileName}`;
-                const fileInfo = await fs.promises.lstat(fileDirectory);
+                const fileDirectory = `${folder}/${fileName}`
+                const fileInfo = await fs.promises.lstat(fileDirectory)
 
                 if (fileInfo.isDirectory()) {
                     // Look through all folders underneath the current directory
                     // Probably very inefficient
                     await this.loadTuneFiles.call(this, fileDirectory) // instead of normal recursion, use function.call to keep class context
-                } else if (fileInfo.isFile() && fileInfo.size === 378) {
-                    // If it's a file and its 378 bytes, it's most likely a tune
+                } else if (fileInfo.isFile() && (fileInfo.size === 378 || fileInfo.size === 386)) {
+                    // If it's a file and its 378 (or 386) bytes, it's most likely a tune
                     const tuneData = await fs.promises.readFile(fileDirectory)
-                    const metadataAPI = new MetadataAPI(folder)
+                    // Tune version check
+                    const newTune = fileInfo.size === 386
+                    if(tuneData[0] !== (newTune ? 3 : 2)) return
+
+                    const metadataAPI = new MetadataAPI(folder, newTune)
                     await metadataAPI.loadMetadata()
                     this.tuneFiles.push({
                         id: fileName,
                         tuneFolderPath: folder,
                         data: tuneData,
                         metadata: metadataAPI
-                    });
+                    })
                 }
             }
+            // Sort by last modified (created?)
+            this.tuneFiles.sort((tune1, tune2) => {
+                const uploadDate1 = new Date(tune1.metadata.getUploadDate())
+                const uploadDate2 = new Date(tune2.metadata.getUploadDate())
+                return uploadDate2 - uploadDate1
+            })
         } catch (error) {
-            console.log(error);
+            console.log(error)
         }
     }
 
